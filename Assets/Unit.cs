09ea0c8ace3e01ee636f14c0b55d1d8a0e3bfc37
as2569 <as2569@njit.edu>
2019-10-26 15:49:59 -0400

    text display and army prefabs

--- Assets/Unit.cs
+++ Assets/Unit.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Unit : MonoBehaviour
 {
     public int health = 3;
+
+    Text healthText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthText = GetComponentInChildren<Text>();
+        healthText.text = health.ToString();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -16,6 +20,7 @@ public class Unit : MonoBehaviour
         if(collision.transform.tag == "Ball")
         {
             health--;
+            healthText.text = health.ToString();
             if (health <= 0)
             {
                 Destroy(gameObject);
