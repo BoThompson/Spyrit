88c2be11b9e8baf803aef81913866981e2ef0615
Bo <AimlessZealot@gmail.com>
2019-10-26 14:56:25 -0400

    Added destroying armies based on mediums dying and fixed some redunant components.

--- Assets/Army.cs
+++ Assets/Army.cs
@@ -2,22 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour
+public class Army : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float speed;
-    public Vector2 direction;
     // Start is called before the first frame update
     void Start()
     {
-        rb.velocity = speed * direction;
+        
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-    }
     // Update is called once per frame
     void Update()
     {
+        
     }
 }
--- Assets/Paddle.cs
+++ Assets/Paddle.cs
@@ -6,12 +6,14 @@ public class Paddle : MonoBehaviour
 {
     public float position;
     public float speed;
+    public float angularSpeed;
     public Vector3 startPosition;
     public Vector3 alphaPosition;
     public Vector3 betaPosition;
     public Vector3 endPosition;
     const float ALPHA = 0.4f;
     const float BETA = 0.6f;
+    public float rotationOffset;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +24,7 @@ public class Paddle : MonoBehaviour
     void Update()
     {
         float movement = 0;
+        float rotation = 0;
         if (Input.GetKey(KeyCode.A))
         {
             movement -= 1;
@@ -32,24 +35,33 @@ public class Paddle : MonoBehaviour
             movement += 1;
         }
 
-        position += movement * speed * Time.deltaTime;
+        if(Input.GetKey(KeyCode.J))
+        {
+            rotation -= 1;
+        }
+        if(Input.GetKey(KeyCode.L))
+        {
+            rotation += 1;
+        }
+        position = Mathf.Clamp(position + movement * speed * Time.deltaTime, 0, 1);
+        rotationOffset = Mathf.Clamp(rotationOffset + rotation * angularSpeed * Time.deltaTime, -30, 30);
         //If position 0 - alpha lerp rotation 0
         if (position <= ALPHA)
         {
             transform.position = Vector3.Lerp(startPosition, alphaPosition, position / ALPHA);
-            transform.rotation = Quaternion.identity;
+            transform.rotation = Quaternion.Euler(0, 0, rotationOffset);
         }
         else if (position <= BETA)
         //If position alpha - beta lerp and rotate
         {
             transform.position = Vector3.Lerp(alphaPosition, betaPosition, (position - ALPHA) / (BETA - ALPHA));
-            transform.rotation = Quaternion.Lerp(Quaternion.identity, Quaternion.Euler(0, 0, 270), (position - ALPHA) / (BETA - ALPHA));
+            transform.rotation = Quaternion.Lerp(Quaternion.Euler(0, 0, rotationOffset), Quaternion.Euler(0, 0, 270 + rotationOffset), (position - ALPHA) / (BETA - ALPHA));
         }
         else
         {
             //If position beta - 1 lerp rotation 90
-            transform.position = Vector3.Lerp(betaPosition, endPosition, position - BETA);
-            transform.rotation = Quaternion.Euler(0, 0, 90);
+            transform.position = Vector3.Lerp(betaPosition, endPosition, (position - BETA) / (1 - BETA));
+            transform.rotation = Quaternion.Euler(0, 0, 270 + rotationOffset);
         }
         
     }
